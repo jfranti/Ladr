2ffcbbbd9ca225e28f3d1cdfe0a6ac55377f3b9e
John Franti <jfranti@gmail.com>
2018-11-16 12:36:59 -0800

    add setup files for projects

---
+++ Ladr.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ladr.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
